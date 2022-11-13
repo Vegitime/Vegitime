e0af3954bd24f861199c46da5c7b0a2c4e6f5ab6
who0803 <10rlatkdtn@naver.com>
2022-11-13 16:19:14 +0900

    feat: default css 추가 및 배경색 설정
    default css 추가
    공통 background 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,68 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+  html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1;
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Source Sans Pro', sans-serif;
+  background-color: #EAF5F7;
+}
+a {
+  text-decoration: none;
+  color: inherit; // 부모에게서 가져와서 링크 색이 변하지 않음
+}
+`;
 
 const App = () => <></>;
 
@@ -9,6 +72,7 @@ if (!rootNode) throw new Error('루트 엘리먼트가 존재하지 않습니다
 
 createRoot(rootNode).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>
 );
