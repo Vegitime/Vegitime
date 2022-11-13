4d8967cd63dd9a36b7052094113b5de03a299adc
GuRaBang <qkrdbsgk00@gmail.com>
2022-11-13 15:46:58 +0900

    fix: react render 방식을 17버전에서 18버전으로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 const App = () => <></>;
 
-render(
+const rootNode = document.getElementById('root');
+
+if (!rootNode) throw new Error('루트 엘리먼트가 존재하지 않습니다.');
+
+createRoot(rootNode).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
