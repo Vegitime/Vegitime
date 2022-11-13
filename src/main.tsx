a232fdffdcd2222ba9f0798c111809f07ca5335e
GuRaBang <qkrdbsgk00@gmail.com>
2022-11-13 15:05:28 +0900

    chore: 기본 레이아웃 작업을 위한 폴더 생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,7 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 
-const App = () => (
-  <figure className="react-figure">
-    <a
-      className="link"
-      href="https://reactjs.org"
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <img className="logo" src="@/assets/logo.svg" alt="React" />
-    </a>
-    <figcaption className="description">React 툴체인 매뉴얼 구성</figcaption>
-  </figure>
-);
+const App = () => <></>;
 
 render(
   <StrictMode>
