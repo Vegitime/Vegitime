41262e08dcfa76d27401765f5fae192362c1126a
who0803 <10rlatkdtn@naver.com>
2022-11-20 06:46:43 +0900

    feat: Signup 페이지 스타일 완성
    Signup 페이지 스타일 완성

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -6,54 +6,151 @@ import { ReactComponent as Formuser } from '@/assets/formuser.svg';
 import { ReactComponent as Padlock } from '@/assets/padlock.svg';
 import { ReactComponent as Name } from '@/assets/name.svg';
 
+const FormContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 390px;
+  min-width: 390px;
+  margin: 140px auto 0;
+  
+  h2 {
+    font-size: 50px;
+    margin-bottom: 40px;
+  }
+`
+
+const Form = styled.form`
+  fieldset {
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    align-items: center;
+  }
+`
+const SignUpErrorMessage = styled.div`
+  width: 100%;
+  position: absolute;
+  margin-left: 30px;
+  font-size: 16px;
+  color: #D9A059;
+`
+
+const FromInputContainer = styled.div`
+  background: #FFFFFF;
+  border-radius: 50px;
+  position: relative;
+  width: 100%;
+  min-width: 390px;
+  height: 60px;
+  &:focus-within {
+    border: 2px solid #5F9538;
+  }
+  svg {
+    position: absolute;
+    left: 30px;
+    top: 12px;
+  }
+
+  ${SignUpErrorMessage} {
+    top: 68px !important;
+  }
+`
+const UserIdCheckButton = styled.button<{isCheckActive: boolean}>`
+  position: absolute;
+  right: 0;
+  width: 68px;
+  height: 100%;
+  border: none;
+  padding: 0;
+  border-radius: 0px 50px 50px 0px;
+  background: ${(props) => (props.isCheckActive ? '#5F9538' : '#A5c597')}; 
+
+  font-size: 16px;
+  color: #FFFFFF;
+`
+
+const FormInput = styled.input`
+  width: 72%;
+  height: 100%;
+  margin-left: 81px;
+  border: none;
+  padding: none;
+  outline: none;
+  font-size: 24px;
+`
+
+
+const FromButton = styled.button<{isActive : boolean}>`
+  width: 100%;
+  height: 60px;
+  background: ${(props) => (props.isActive ? '#5F9538' : '#A5c597')}; 
+  border: none;
+  border-radius: 50px;
+
+  font-size: 24px;
+  color: #FFFFFF;
+`
+
+const FormTip = styled.div`
+  margin-top: 24px;
+  font-size: 20px;
+  display: flex;
+  gap: 19px;
+  a {
+    color: #2E6316;
+  }
+`
+
 
 export default function SignUp() {
   return (
     <>
-      <div>
+      <FormContainer>
         <h2>Sign up</h2>
-        <form>
+        <Form>
           <fieldset>
             <legend className="sr-only">회원 로그인 폼</legend>
             
-            <div>
+            <FromInputContainer>
               <label className="sr-only" htmlFor="userId">아이디</label>
               <Formuser fill='#01192C' width="35" height="35"/>
-              <input type="text" id="userId" required placeholder="아이디를 입력해주세요"/>
-              <div>영문, 숫자 포함 6~15자로 입력해주세요.</div>
-              <button>완료!</button>
-            </div>
+              <FormInput type="text" id="userId" required placeholder="아이디"/>
+              <SignUpErrorMessage>영문, 숫자 포함 6~15자로 입력해주세요.</SignUpErrorMessage>
+              <UserIdCheckButton type="button" isCheckActive={true}>완료!</UserIdCheckButton>
+            </FromInputContainer>
 
-            <div>
+            <FromInputContainer>
               <label className="sr-only" htmlFor="userNickName">닉네임</label>
               <Name fill='#01192C' width="35" height="35"/>
-              <input type="text" id="userNickName" required placeholder="닉네임을 입력해주세요"/>
-              <div>1글자 이상 입력해주세요.</div>
-            </div>
+              <FormInput type="text" id="userNickName" required placeholder="닉네임"/>
+              <SignUpErrorMessage>1글자 이상 입력해주세요.</SignUpErrorMessage>
+            </FromInputContainer>
             
-            <div>
+            <FromInputContainer>
               <label className="sr-only" htmlFor="userPwd">비밀번호</label>
               <Padlock fill='#01192C' width="35" height="35"/>
-              <input type="password" id="userPwd" required placeholder="패스워드를 입력해주세요"/>
-              <div>영문, 숫자 포함 6~10자로 입력해주세요.</div>
-            </div>
+              <FormInput type="password" id="userPwd" required placeholder="패스워드"/>
+              <SignUpErrorMessage>영문, 숫자 포함 6~10자로 입력해주세요.</SignUpErrorMessage>
+            </FromInputContainer>
 
-            <div>
+            <FromInputContainer>
               <label className="sr-only" htmlFor="userPwdCheck">비밀번호 확인</label>
               <Padlock fill='#01192C' width="35" height="35"/>
-              <input type="password" id="userPwdCheck" required placeholder="패스워드를 한 번 더 입력해주세요"/>
-              <div>비밀번호가 일치하지 않아요.</div>
-            </div>
+              <FormInput type="password" id="userPwdCheck" required placeholder="패스워드 재확인"/>
+              <SignUpErrorMessage>비밀번호가 일치하지 않아요.</SignUpErrorMessage>
+            </FromInputContainer>
             
-            <button type="submit">회원가입</button>
+            <FromButton isActive={true} type="submit">회원가입</FromButton>
           </fieldset>
-        </form>
+        </Form>
 
-        <div>
+        <FormTip>
           <span>아직 회원이세요?</span>
           <Link to="/">로그인</Link>
-        </div>
-      </div>
+        </FormTip>
+      </FormContainer>
       <FooterImg type="start" />
     </>
   );
