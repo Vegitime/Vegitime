f94de5c591986486baa19d7846870afd41305843
GuRaBang <qkrdbsgk00@gmail.com>
2023-01-03 23:29:14 +0900

    refactor: Header 컴포넌트 위치 변경

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,11 @@
-import { MoneyInfo } from './index';
 import styled from 'styled-components';
 import { flexContainer } from 'styles';
 import { getAsset } from 'utils';
+import { MoneyInfo } from 'components';
 
 const StyledHeader = styled.header`
   ${flexContainer({ d: 'row', w: 'nowrap', jc: 'space-between' })}
-  padding: 0 var(--spacing-base);
+  margin: 0 var(--spacing-base);
 `;
 
 const StyledDiv = styled.div`
@@ -17,11 +17,7 @@ export default function Header() {
     <StyledHeader>
       <img src={getAsset('home.png')} alt="메인 페이지 이동" />
       <StyledDiv>
-        <MoneyInfo
-          fontSize="var(--text-sm)"
-          gap="var(--spacing-xxxs)"
-          size="2.25rem"
-        />
+        <MoneyInfo size="large">{2323200}</MoneyInfo>
         <img src={getAsset('logout.png')} alt="로그아웃" />
       </StyledDiv>
     </StyledHeader>
