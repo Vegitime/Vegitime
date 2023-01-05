5cf7ea3b4c4e603c3310b710a56249d8cccd23fb
OrangeChick <retww@naver.com>
2023-01-06 01:20:57 +0900

    refactor: ProgressBar에 global style 적용 및 스타일 변경

--- client/src/pages/VegiPage/components/ProgressBar.tsx
+++ client/src/pages/VegiPage/components/ProgressBar.tsx
@@ -6,39 +6,40 @@ interface TextType {
 
 const Container = styled.div`
   position: relative;
-  width: 353px;
-  margin: 0 auto 32px;
+  width: 100%;
+  margin: 0 auto 2rem;
 `;
 
 const BackBar = styled.div`
-  width: 353px;
-  height: 30px;
-  background-color: #ffffff;
-  border-radius: 50px;
+  width: 100%;
+  height: 1.875rem;
+  background-color: var(--color-white);
+  border-radius: 3.125rem;
   margin: 0 auto;
 `;
 
 const FrontBar = styled.div`
-  width: calc(353px * (3 / 5));
-  height: 30px;
-  background-color: #a4c597;
+  width: calc(100% * (3 / 5));
+  height: 1.875rem;
+  background-color: var(--color-light-green);
   position: absolute;
   top: 0px;
   left: 0px;
-  border-radius: 50px;
+  border-radius: 3.125rem;
   margin: 0 auto;
 `;
 
 const P = styled.p`
   width: 100%;
   font-size: var(--spacing-base);
-  color: #5f9538;
+  color: var(--color-normal-green);
   position: absolute;
-  top: 0px;
+  top: 50%;
+  transform: translateY(-50%);
   left: 0px;
   text-align: center;
   margin: 0;
-  line-height: 3.75rem;
+  line-height: 1;
 `;
 
 export default function ProgressBar({ text }: TextType) {
