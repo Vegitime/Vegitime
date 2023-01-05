2785c5fd2956d4cecd83aa521abb2c886818d19f
OrangeChick <retww@naver.com>
2023-01-06 01:23:56 +0900

    refactor: VegiPage 구조 변경 및 스타일 변경

--- client/src/pages/VegiPage/index.tsx
+++ client/src/pages/VegiPage/index.tsx
@@ -1,24 +1,33 @@
 import styled from 'styled-components';
-import { Title, Time, VegiImage, ProgressBar } from './components';
+import {
+  Title,
+  Time,
+  VegiImage,
+  ProgressBar,
+  ActionButton,
+} from './components';
 import { Header, Navigation } from 'components';
 
 const Container = styled.div`
   position: relative;
   min-height: 100vh;
-  padding: 2rem 1rem 1rem;
-  background: #eaf5f7;
+  padding: 1rem;
+  background: var(--color-skyblue);
   text-align: center;
 `;
 
 export default function VegiPage() {
   return (
-    <Container>
+    <>
       <Header />
-      <Title level="Level. 3" name="가지 가지" />
-      <ProgressBar text="3 / 5" />
-      <VegiImage />
-      <Time text="AM 07:00" />
+      <Title name="가지 가지" />
+      <Container>
+        <ProgressBar text="3 / 5" />
+        <VegiImage />
+        <Time text="AM 07:00" />
+        <ActionButton action="voice" />
+      </Container>
       <Navigation />
-    </Container>
+    </>
   );
 }
