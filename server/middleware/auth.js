126984ee3f7a89db579d623e186b8938c3c42f2c
who0803 <10rlatkdtn@naver.com>
2023-01-07 18:29:58 +0900

    feat: aut인증 처리 미들웨어 생성
    - auth 인증 처리
    - 클라이언트 쿠키에서 토큰을 가져온다

---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+const { User } = require('../models/User');
+const { generateResponse } = require("../utils/generateResponse");
+
+let auth = (req, res, next) => {
+
+    // 인증 처리를 하는 곳
+    // 클라이언트 쿠키에서 토큰을 가져온다.
+    let token = req.cookies.auth_token;
+    User.findByToken(token, (err, user) => {
+        if(err) return res.json(generateResponse(500, false, err));
+        if(!user) return res.json(generateResponse(500, false, null, null, false));  // 유저 못찾으면 리턴
+
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = { auth };
