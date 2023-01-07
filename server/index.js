ae97dddc591d8b375d2ec6b288886a1acd378034
who0803 <10rlatkdtn@naver.com>
2023-01-07 18:14:16 +0900

    feat: 서버 기본 설정 in server/index.js
    - express로 서버 구축
    - DB 연결
    - 몽구스 설정
    - cors 설정

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended: true }));  
+app.use(express.json());  
+app.use(cookieParser());  
+
+// cors 설정
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin(origin, callback) {
+    const issafesitelisted = whitelist.indexOf(origin) !== -1;
+    callback(null, issafesitelisted);
+  },
+  credentials: true,
+  optionsSuccessStatus: 200, 
+};
+app.use(cors(corsOptions));
+
+// db 연결
+const mongoose = require('mongoose');
+const connect = mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}).then(() => {console.log('MongoDB Connected...')}).catch(err => console.log(err));
+
+//use this to show the image you have in node js server to client (react js)
+app.use('/public', express.static('public'));
+
+const port = process.env.PORT || 8080
+app.listen(port, function() {
+  console.log(`listening on ${port}`);
+})
