dadcdc7360601f13fc6d2491d04306887a648555
OrangeChick <retww@naver.com>
2023-01-09 01:03:08 +0900

    feat: 음성인식 기능 구현

--- client/src/pages/VegiPage/components/index.ts
+++ client/src/pages/VegiPage/components/index.ts
@@ -1,6 +1,6 @@
 import Time from './Time';
 import VegiImage from './VegiImage';
 import ProgressBar from './ProgressBar';
-import ActionButton from './ActionButton';
+import DictButton from './DictButton';
 
-export { Time, VegiImage, ProgressBar, ActionButton };
+export { Time, VegiImage, ProgressBar, DictButton };
