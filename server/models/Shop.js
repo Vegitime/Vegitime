72a687e0bab6acaae6031728cde9adb1b0c2df3a
who0803 <10rlatkdtn@naver.com>
2023-01-08 23:00:46 +0900

    feat: Vegetable, Shop 스키마 생성
    - Vegetable 스키마 생성
    - Shop 스키마 생성

---
+++ server/models/Shop.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const imageSchema = new mongoose.Schema({
+  1: String,
+  2: String,
+  3: String,
+  4: String,
+  5: String,
+});
+
+const shopSchema = mongoose.Schema({
+  name: String,  // 야채 이름
+  purchasePrice: Number,  // 야채 구매 가격
+  sellingPrice: Number, // 야채 판매 가격
+  description: String,  // 야채 특징 설명
+  image: imageSchema, // 레벨별 이미지
+})
+
+const Shop = mongoose.model('Shop', shopSchema)
+module.exports = { Shop }
