544141b484c1b1e80ce592c193061e33f2e61b81
OrangeChick <retww@naver.com>
2023-01-10 10:07:49 +0900

    refactor: 불필요한 컴포넌트 삭제

--- client/src/pages/VegiPage/components/index.ts
+++ client/src/pages/VegiPage/components/index.ts
@@ -1,6 +1,5 @@
 import Time from './Time';
-import VegiImage from './VegiImage';
 import ProgressBar from './ProgressBar';
 import DictButton from './DictButton';
 
-export { Time, VegiImage, ProgressBar, DictButton };
+export { Time, ProgressBar, DictButton };
