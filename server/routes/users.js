5207369ea72cc506468ffe755efda622940d5ec8
who0803 <10rlatkdtn@naver.com>
2023-01-10 11:23:59 +0900

    feat: user의 정보 받아오는 api 추가
    - user의 정보를 받아오는 api
    	- 유저의 고유 아이디로 vegis 배열 갖고 온다.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require("../models/User");
+const { Vegetable } = require("../models/Vegetable");
 const { auth } = require("../middleware/auth");
 const { generateResponse } = require("../utils/generateResponse");
 
@@ -9,7 +10,6 @@ router.get("/auth", auth, (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-    console.log('register: ', req.body)
     const user = new User(req.body);
     user.save((err, doc) => {
         if (err) {
@@ -54,8 +54,29 @@ router.get("/logout", auth, (req, res) => {
         if (err) return res.json(generateResponse(500, false, err));
 
         res.clearCookie('auth_token');
-        return res.status(200).json(generateResponse(201, true));
+        return res.status(200).json(generateResponse(200, true));
     });
+});
+
+router.get("/info", auth, (req, res) => {
+    // user 고유 아이디로 vegis 배열 가져오기
+    // 그 다음 가공해서 리턴
+    Vegetable.find({ "ownerId": req.user._id })  // user의 고유 아이디를 갖는 야채 찾기
+        .exec((err, info) => {
+            if (err) return res.json(generateResponse(500, false, err));
+            const data = {
+                nickname: req.user.nickname,
+                money: req.user.money,
+                harvest: req.user.harvest
+            }
+            data.vegis = info.map(info => ({
+                name: info.name,
+                level: info.level,
+                alarm: info.alarm,
+                attendance: info.attendance
+            }))
+            res.status(200).json(generateResponse(200, true, null, null, null, data));
+        })
 });
 
 module.exports = router;
