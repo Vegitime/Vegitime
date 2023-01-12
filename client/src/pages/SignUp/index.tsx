512de793dcfdc3e00f9120b990c0dbf7ba1338ce
who0803 <10rlatkdtn@naver.com>
2023-01-12 18:28:51 +0900

    feat: SignUP 페이지 구현
    - Signup 페이지 구현
    - useForm을 이용하여 form 로직 구현
    - api 요청
    
    	- 회원가입 요청시
    		- 성공 : signin 페이지로 라우팅
    		- 실패 alert
    	- 중복확인 요청시
    		- 결과값을 isDuplicated state에 설정

--- client/src/pages/SignUp/index.tsx
+++ client/src/pages/SignUp/index.tsx
@@ -1,7 +1,14 @@
 /* eslint-disable prettier/prettier */
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import FooterImg from '../StartPage/components/FooterImg';
-import { SignForm, SignTip } from './components';
+import { ReactComponent as Formuser } from '@/assets/formuser.svg';
+import { ReactComponent as Padlock } from '@/assets/padlock.svg';
+import { ReactComponent as Name } from '@/assets/name.svg';
+import { TextButton, Form, InputContainer, Input, ErrorMessage, SignTip, FooterImg } from 'components';
+import { DuplicationButton } from './components';
+import { useForm } from 'hooks';
+import { useState } from 'react';
+import axios from 'axios';
 
 const FormContainer = styled.div`
   position: relative;
@@ -29,19 +36,97 @@ const FormContainer = styled.div`
 `
 
 export default function SignUp() {
+  const navigate = useNavigate();
+  const [isDuplicated, setIsDuplicated] = useState<boolean | null>(null);
+  const { values, errors, touched, handleSubmit, getFieldProps, isValid } = useForm({
+    initialValues: { id: "", nickname: "", password: "", pwcheck: "" },
+    validate: values => {
+      const errors = {
+        id: "",
+        nickname: "",
+        password: "",
+        pwcheck: "",
+      }
+      if (!/^[a-z|A-Z|0-9|]{6,15}$/.test(values.id)) {
+        errors.id = "영문, 숫자 포함 6~15자로 입력해주세요."
+      }
+      if (!values.nickname) {
+        errors.nickname = "1글자 이상 입력해주세요."
+      }
+      if (!/^[a-z|A-Z|0-9|]{6,10}$/.test(values.password)) {
+        errors.password = "영문, 숫자 포함 6~10자로 입력해주세요."
+      }
+      if (values.password !== values.pwcheck) {
+        errors.pwcheck = "비밀번호가 일치하지 않아요."
+      }
+      setIsDuplicated(null)
+      return errors
+    },
+    onSubmit: async values => {
+      try {
+        const result = await axios.post(`${process.env.URL}api/users/register`, {id: values.id, nickname: values.nickname, password: values.password}, {withCredentials: true}).then(response => response.data);
+        if (result.body.success) navigate('/signin')
+        else alert('회원가입에 실패하였습니다. 다시 시도해주세요.')
+      }
+      catch(err) {
+        console.error(err)
+      }
+    },
+  })
+  const checkDuplication = async () => {
+    try {
+      const result = await axios.post(`${process.env.URL}api/users/duplication`, {id: values.id}, {withCredentials: true}).then(response => response.data);
+      setIsDuplicated(!result.body.success)
+    }
+    catch(err) {
+      console.error(err)
+    }
+  }
+  const getSVGColor = (content:string) => content === '' ? '#CDCDCD' : '#01192C'; // 유틸로
+  const isCompValid = () => isDuplicated === false && isValid()
   return (
     <>
       <FormContainer>
         <h2>Sign up</h2>
-        <SignForm formType='signUp'/>
+        <Form handleSubmit={handleSubmit}>
+          <fieldset>
+            <legend className="sr-only">회원가입 폼</legend>
+            <InputContainer>
+              <Formuser fill={getSVGColor(values.id)} width="35" height="35"/>
+              <Input type="text" name="id" placeholder="아이디를 입력해주세요" autoComplete ="off" getFieldProps={getFieldProps} />
+              <DuplicationButton type="button" isCheckActive={errors.id === ''} onClick={checkDuplication}>{isDuplicated === false ? "완료!" : "중복확인"}</DuplicationButton>
+              <ErrorMessage name="id" touched={touched} errors={errors} isDuplicated={isDuplicated} style={{color:'--color-gold'}}></ErrorMessage>
+            </InputContainer>
+            <InputContainer>
+              <Name fill={getSVGColor(values.nickname)} width="35" height="35"/>
+              <Input type="text" name="nickname" placeholder="닉네임을 입력해주세요" autoComplete ="off" getFieldProps={getFieldProps} />
+              <ErrorMessage name="nickname" touched={touched} errors={errors} style={{color:'--color-gold'}}></ErrorMessage>
+            </InputContainer>
+            <InputContainer>
+              <Padlock fill={getSVGColor(values.password)} width="35" height="35"/>
+              <Input type="password" name="password" placeholder="패스워드를 입력해주세요" autoComplete ="off" getFieldProps={getFieldProps} />
+              <ErrorMessage name="password" touched={touched} errors={errors} style={{color:'--color-gold'}}></ErrorMessage>
+            </InputContainer>
+            <InputContainer>
+              <Padlock fill={getSVGColor(values.pwcheck)} width="35" height="35"/>
+              <Input type="password" name="pwcheck" phSize={'sm'} placeholder="패스워드를 한 번 더 입력해주세요" autoComplete ="off" getFieldProps={getFieldProps} />
+              <ErrorMessage name="pwcheck" touched={touched} errors={errors} style={{color:'--color-gold'}}></ErrorMessage>
+            </InputContainer>
+            <TextButton
+              type="submit"
+              width="24.375rem"
+              size="large"
+              backgroundColor={'var(--color-normal-green)'}
+              disabledBackgroundColor={'var(--color-light-green)'}
+              disabled={!isCompValid()}
+            >회원가입</TextButton>
+          </fieldset>
+        </Form>
         <SignTip formType='signUp'/>
       </FormContainer>
       <FooterImg type="start" />
     </>
   );
 }
-
-
-
 
 
