5bf87dfd0f4bf9125e45b4d8279c959e284f894b
GuRaBang <87111950+GuRaBang@users.noreply.github.com>
2023-01-14 14:39:47 +0900

    chore: prettier 옵션 추가 style: prettier 옵션 추가로 인한 코드 스타일 변경 feat: 스키마 및 요청 응답에 type 추가

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
+
 const app = express();
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 require('dotenv').config();
 
-app.use(express.urlencoded({ extended: true }));  
-app.use(express.json());  
-app.use(cookieParser());  
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 // cors 설정
 const whitelist = ['http://localhost:3000'];
@@ -16,23 +17,29 @@ const corsOptions = {
     callback(null, issafesitelisted);
   },
   credentials: true,
-  optionsSuccessStatus: 200, 
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
 // db 연결
 const mongoose = require('mongoose');
-const connect = mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}).then(() => {console.log('MongoDB Connected...')}).catch(err => console.log(err));
+
+const connect = mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected...');
+  })
+  .catch((err) => console.log(err));
 
 // api 라우터 설정
 app.use('/api/users', require('./routes/users'));
 app.use('/api/vegetables', require('./routes/vegetables'));
 app.use('/api/shop', require('./routes/shop'));
 
-//use this to show the image you have in node js server to client (react js)
+// use this to show the image you have in node js server to client (react js)
 app.use('/public', express.static('public'));
 
-const port = process.env.PORT || 8080
-app.listen(port, function() {
+const port = process.env.PORT || 8080;
+app.listen(port, function () {
   console.log(`listening on ${port}`);
-})
+});
